16ce0884e49c3c2bdf7a0918b5bb72a4f6823432
Zoe Gelech <archaic.ghost@gmail.com>
2018-09-04 21:38:01 +0000

    Fill Out urls_Index.ejs Template

--- express_server.js
+++ express_server.js
@@ -5,8 +5,8 @@ var PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+ "b2xVn2": "http://www.lighthouselabs.ca",
+ "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
@@ -14,7 +14,20 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase };
+    // res.send(req.params.id)
+    let templateVars = { urls : urlDatabase};
+    for(let key in urlDatabase) {
+        console.log(urlDatabase[key])
+    }
+
+app.get("/urls/:id", (req, res) => {
+    // res.send(req.params.id)
+    let id = req.param('id')
+    let templateVars = { shortURL: req.params.id };
+    res.render("urls_show", templateVars);
+    });
+    
+
     res.render("urls_index", templateVars);
   });
 
