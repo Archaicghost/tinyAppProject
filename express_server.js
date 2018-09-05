d0fe89e2cee8680e0f8b8ed805664a630cf630f1
Zoe Gelech <archaic.ghost@gmail.com>
2018-09-05 01:48:48 +0000

    random number and letter genorator

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+
 var urlDatabase = {
  "b2xVn2": "http://www.lighthouselabs.ca",
  "9sm5xK": "http://www.google.com"
@@ -14,12 +15,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-    // res.send(req.params.id)
     let templateVars = { urls : urlDatabase};
-    // for(let key in urlDatabase) {
-    //     console.log(urlDatabase[key])
     res.render("urls_index", templateVars)
     });
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+    
+app.get("/urls/new", (req, res) => {
+res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
     let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
@@ -27,7 +32,6 @@ app.get("/urls/:id", (req, res) => {
     res.render("urls_show", templateVars);
     
     });
-  
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -37,5 +41,29 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.post("/urls", (req, res) => {
+    console.log(req.body[`longURL`]);  // debug statement to see POST parameters
+    res.send(generateRandomString(req.body));         // Respond with 'Ok' (we will replace this)
+  });
+
+
+  function generateRandomString() {
+    let randomKey = "";
+    let character = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+for (let i = 0; i < 6 ; i++) {
+  randomKey += character.charAt(Math.floor(Math.random() * character.length));
+}
+return randomKey;
+}
+
+
+app.get("/u/:shortURL", (req, res) => {
+    // let longURL = 
+    res.redirect()
+  });
+
+
+//recieve new url
+//attach random string to url
+//add url and random string to database
+//respond with redirection to shortURL
