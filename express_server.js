f7c0ca79204acf6fa6ba8ad1cbbc7d9b7d5cbfb0
Zoe Gelech <archaic.ghost@gmail.com>
2018-09-05 22:19:25 +0000

    Adding edit and delete features

--- express_server.js
+++ express_server.js
@@ -3,51 +3,55 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+//middleware
 app.set("view engine", "ejs");
 
-
+//Database of urls 
 var urlDatabase = {
  "b2xVn2": "http://www.lighthouselabs.ca",
  "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
+//list of all urls
 app.get("/urls", (req, res) => {
     let templateVars = { urls : urlDatabase};
     res.render("urls_index", templateVars)
     });
 
+    //Accept Form info
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-    
+
+//Create New Urls
 app.get("/urls/new", (req, res) => {
 res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
     let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+
     res.render("urls_show", templateVars);
-    
     });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
+    
   
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+//Add new urls with create url
 app.post("/urls", (req, res) => {
      
     var makeShortUrl = generateRandomString();
+    var myShortURL = makeShortUrl
     var makeLongString = req.body['longURL']
     urlDatabase[makeShortUrl] = makeLongString
-    res.redirect("/urls/" + makeShortUrl)
+    res.redirect("/urls/" + myShortURL)
     // res.send(makeShortUrl + ": " + req.body['longURL']);     
   });
 
@@ -61,10 +65,25 @@ for (let i = 0; i < 6 ; i++) {
 return randomKey;
 }
 
-app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-     res.redirect(longURL);
-   });
+
+   // Delete existing url
+   app.post('/urls/:id/delete', (req, res) => {
+    let targetId = req.params.id;
+  delete urlDatabase[targetId]
+    res.redirect('/urls');
+  });
+
+
+  app.post('/urls/:id/edit', (req, res)=> {
+    let targetId = req.params.id;
+    urlDatabase[targetId] = req.body['longURL']
+    res.redirect('/urls/')
+  });
+  
+
+
+
+// })
 
 
 //recieve new url
